5e4b8ad9734fab3e736886753a1a153cbbdbefd3
Derek <darmfield2023@gmail.com>
2022-12-08 09:46:57 -0500

    compiled background removal and data loading in single package for ease of use following API call

--- python/remove_background.py
+++
@@ -1,8 +0,0 @@
-from rembg import remove
-from PIL import Image
-import os
-
-input_path = 'dumbass.jpeg'
-input = Image.open('dumbass.jpeg')
-output = remove(input)
-output.save(os.path.split(input_path))
